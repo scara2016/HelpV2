316bc06c876b666af5e3a9f996da0f026901ec89
coolevil98 <68813255+coolevil98@users.noreply.github.com>
2022-04-20 11:47:33 +1000

    Created level 1, scenemanager
    Created a basic level 1 and created script to go between levels

---
+++ HelpUs/Assets/Scripts/TeamCollider.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeamCollider : MonoBehaviour
+{
+
+    private Transform target; 
+    private int nextScene;
+
+     private string SceneFormat= "Level{0}";
+    private string SceneNumber;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player1");
+        if(player!=null)
+        {
+            target=player.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        GameObject other = collider.gameObject;
+        Transform seeIfTeam=collider.transform;
+
+        if(seeIfTeam==target)
+        {
+            nextScene=GameManager.Instance.GetNextScene();
+            SceneNumber= string.Format(SceneFormat, nextScene);
+            GameManager.Instance.NextScene(SceneNumber);
+        }
+    }
+}
