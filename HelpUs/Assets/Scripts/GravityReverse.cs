36c24fff29f29707eabe81be4c553e67e46541d3
coolevil98 <68813255+coolevil98@users.noreply.github.com>
2022-04-20 13:08:23 +1000

    Started gravity reverse script

---
+++ HelpUs/Assets/Scripts/GravityReverse.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityReverse : MonoBehaviour
+{
+
+    private MovementManager movementManager;
+    public float convertDirection=9.8f;
+
+    private void Awake() {
+        movementManager = FindObjectOfType<MovementManager>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void ChangeDirection()
+    {
+        convertDirection*=-1
+        Physics2D.gravity = new Vector2(0, convertDirection);
+    }
+
+
+}
