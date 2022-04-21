176b818582d67a55e249a0f745c209f905f0bb37
AussieGarlic <69225639+AussieGarlic@users.noreply.github.com>
2022-04-21 10:16:39 +1000

    door fix, level 4 edits

--- HelpUs/Assets/Scripts/TrapDoorMovement.cs
+++ HelpUs/Assets/Scripts/TrapDoorMovement.cs
@@ -8,29 +8,30 @@ public class TrapDoorMovement : MonoBehaviour
     public Buttons activatorButton;
     public Transform StartPoint;
     public Transform EndPoint;
-    public float timeToReachTarget =2;
+    public float timeToReachTarget = 2;
     private float t;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveActivated = activatorButton.ButtonOn;
     }
 
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime / timeToReachTarget; 
+        t += Time.deltaTime / timeToReachTarget;
         moveActivated = activatorButton.ButtonOn;
+        
         if (moveActivated)
         {
             this.transform.transform.position = Vector3.Lerp(StartPoint.position, EndPoint.position, t);
         }
 
-        if(!moveActivated)
-        {
-            this.transform.transform.position = Vector3.Lerp(EndPoint.position, StartPoint.position, t);
-        }
+        // if(!moveActivated)
+        // {
+        //     this.transform.transform.position = Vector3.Lerp(EndPoint.position, StartPoint.position, t);
+        // }
     }
 
 }
